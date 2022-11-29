538481f096feb7d716284f2e2b99f16ce25b3da5
Haiki <kitanaid125@gmail.com>
2022-11-30 01:49:39 +0200

    login page

--- MyBreakingBadSite/pages/login.aspx.cs
+++ MyBreakingBadSite/pages/login.aspx.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace MyBreakingBadSite.pages
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
+        public string st = "";
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            if (Request.Form["validation"] != null)
+            {
+                string email = Request.Form["email"];
+                string password = Request.Form["password"];
+
+                <
+               
+
+            }
+
 
         }
+        
     }
 }
